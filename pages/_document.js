d987097c28b9869ffce24980ccc0ec54dff7a539
김홍록 | HongRok <dev@hongrok.net>
2022-12-21 10:54:13 +0900

    pretendard jsdelivr css 로 인한 tailwind css 이슈 해결

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="ko">
       <meta name="theme-color" content="white" />
+      <link
+        rel="stylesheet"
+        as="style"
+        crossOrigin
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"
+      />
       <Head />
       <body>
         <Main />
